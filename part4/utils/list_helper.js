0a12b0f70e1a80fe78ab9133e2c431f489265286
JD Pravin <dogfly6@yahoo.com>
2024-07-31 12:42:36 -0500

    exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
     return 1
   }
-  
-  module.exports = {
-    dummy
-  }
+
+const totalLikes = (blogs) => {
+    return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog)=>{return sum + blog.likes}, 0)
+}
+
+module.exports = {
+dummy, totalLikes
+}
